266a4afcb49a41ec6eaf3aac813f9b1c1878ae18
João Paulo Maciel <joao.carvalho@pensiondynamics.com.br>
2023-07-02 21:24:52 -0300

    Fix: loading

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import store from '../store';
@@ -7,12 +7,27 @@ import Navbar from '../components/navbar';
 import theme from '../utils/theme/theme';
 
 export default function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 500);
+  }, []);
+
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <Component {...pageProps} store={store} />
-      </ThemeProvider>
+      {
+        loading ? (
+          <ThemeProvider theme={theme}>
+            <Navbar />
+            <Component {...pageProps} store={store} />
+          </ThemeProvider>
+        )
+          :
+          <h1>.</h1>
+      }
+
     </Provider>
   );
 }
